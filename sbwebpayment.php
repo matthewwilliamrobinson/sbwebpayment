3204cd19bd27bd4be0aae7b5e3706b137f3d4523
Rogelio Castillo <rogelio.castillo@sbwebstore.com>
2016-06-29 18:31:26 +0000

    update

--- sbwebpayment.php
+++ sbwebpayment.php
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2015 PrestaShop
+* 2007-2016 Sbwebstore LLC.
 *
 * NOTICE OF LICENSE
 *
@@ -18,8 +18,8 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2015 PrestaShop SA
+*  @author Sbwebstore LLC.
+*  @copyright  2007-2016 Sbwebstore LLC.
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
@@ -96,6 +96,7 @@ class SbWebPayment extends Module
         $default_lang = (int)Configuration::get('PS_LANG_DEFAULT');
 
         // Init Fields form array
+        $fiels_form
         $fields_form[0]['form'] = array(
             'legend' => array(
                 'title' => $this->l('Settings'),
--- views/templates/hook/sbwebpayment.tpl
+++ views/templates/hook/sbwebpayment.tpl
@@ -1,6 +1,6 @@
 <?php
 /*
-* 2007-2015 PrestaShop
+* 2007-2016 Sbwebstore LLC.
 *
 * NOTICE OF LICENSE
 *
@@ -18,8 +18,8 @@
 * versions in the future. If you wish to customize PrestaShop for your
 * needs please refer to http://www.prestashop.com for more information.
 *
-*  @author PrestaShop SA <contact@prestashop.com>
-*  @copyright  2007-2015 PrestaShop SA
+*  @author Sbwebstore LLC.
+*  @copyright  2007-2016 Sbwebstore LLC.
 *  @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
 *  International Registered Trademark & Property of PrestaShop SA
 */
